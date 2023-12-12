b522898561705d399ded265a37e56a3d2aef0a85
fernandeezz <fernandafernandes2020@outlook.com.br>
2023-12-12 15:00:59 -0300

    Error e Autenticação

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -34,7 +35,7 @@ class SupportController extends Controller
         return view('admin.supports.create');
     }
 
-    public function store(Request $request, Support $support){
+    public function store(StoreUpdateSupport $request, Support $support){
 
         $data = $request->except('_token'); // Exclua o campo _token antes de criar o modelo
     
--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,5 +1,13 @@
 <h1>Novo Suporte</h1>
 
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('supports.store') }}" method="post">
     @csrf <!-- Use a diretiva para incluir automaticamente o token CSRF -->
     <input type="text" name="subject" placeholder="Assunto">
